244fe4cb452ee513ec461abd2e99631ed58e7761
sparrowcms01 <148522357+sparrowcms01@users.noreply.github.com>
2024-01-10 11:16:03 +0530

    Update Leaseing.js

--- src/views/source/Leaseing.js
+++ src/views/source/Leaseing.js
@@ -1677,7 +1677,7 @@ const Leaseing = () => {
       dayFrequency: selectedDayFrequency,
       ccnumber: card_number || "",
       email: tenantsSchema.values.tenant_email,
-      ccexp: exp_date ? formatDateForInput(exp_date) : "",
+      ccexp: exp_date ,
       first_name: tenantsSchema.values.tenant_firstName,
       last_name: tenantsSchema.values.tenant_lastName,
       address: entrySchema.values.rental_adress,
