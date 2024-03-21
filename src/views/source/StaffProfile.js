59e1802950cff3d0c73eab36e9f66374c2be4d7e
KrushilVaghani <krushilvaghani1227@gmail.com>
2024-03-21 11:50:15 +0530

    krushil212121

--- src/views/source/StaffProfile.js
+++ src/views/source/StaffProfile.js
@@ -15,6 +15,13 @@ import {
 } from "reactstrap"; // Import other necessary components from 'reactstrap'
 import { jwtDecode } from "jwt-decode";
 
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 const StaffProfile = () => {
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const { id } = useParams();
@@ -58,6 +65,23 @@ const StaffProfile = () => {
     getStaffData();
   }, [accessType]);
 
+  function createData(
+    // name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    protein: number
+  ) {
+    return { calories, fat, carbs, protein };
+  }
+  const rows = [
+    createData(159, 6.0, 24, 4.0),
+    createData(237, 9.0, 37, 4.3),
+    createData(262, 16.0, 24, 6.0),
+    createData(305, 3.7, 67, 4.3),
+    createData(356, 16.0, 49, 3.9),
+  ];
+
   return (
     <>
       <StaffHeader />
@@ -65,72 +89,55 @@ const StaffProfile = () => {
       <Container className="mt--7" fluid>
         <Row>
           <div className="col">
-            <Card className="shadow" style={{ backgroundColor: "#FFFEFA" }}>
-              <CardHeader className="border-0">
-                <h2 className="mb-0" style={{ color: "#033E3E" }}>
-                  Personal Details
-                </h2>
-              </CardHeader>
-              <div className="table-responsive" style={{ padding: "15px" }}>
-                <Table
-                  className="align-items-center table-flush"
-                  responsive
-                  style={{ width: "100%" }}
-                >
-                  {loading ? (
-                    <div className="d-flex flex-direction-row justify-content-center align-items-center p-5 m-5">
-                      <RotatingLines
-                        strokeColor="grey"
-                        strokeWidth="5"
-                        animationDuration="0.75"
-                        width="50"
-                        visible={loading}
-                      />
-                    </div>
-
-                  ) : (
-                    // ) : staffDetails?.staffmember_id ? (
-                    <>
-                      <tbody
-                        className="tbbody p-0 m-0"
-                        style={{
-                          borderTopRightRadius: "5px",
-                          borderTopLeftRadius: "5px",
-                          borderBottomLeftRadius: "5px",
-                          borderBottomRightRadius: "5px",
-                        }}
-                      >
-                        <tr className="header">
-                          <th>First name</th>
-                          <th>Designation</th>
-                          <th>Phone Number</th>
-                          <th>Email</th>
-                        </tr>
-                        <>
-                          <>
-                            {!staffDetails ? (
-                              <tbody>
-                                <tr className="text-center">
-                                  <td colSpan="8" style={{ fontSize: "15px" }}>No Data Added</td>
-                                </tr>
-                              </tbody>
-                            ) : (
-
-                              <tr className="body">
-                                <td>{staffDetails?.staffmember_name}</td>
-                                <td>{staffDetails?.staffmember_designation}</td>
-                                <td>{staffDetails?.staffmember_phoneNumber}</td>
-                                <td>{staffDetails?.staffmember_email}</td>
-                              </tr>
-                            )}
-                          </>
-                        </>
-                      </tbody>
-                    </>
-                  )}
-                </Table>
-              </div>
+            <Card
+              className="shadow"
+              style={{
+                backgroundColor: "#152B51",
+                padding: "20px",
+                borderRadius: "20px",
+              }}
+            >
+              <h2 className="mb-0" style={{ color: "white" }}>
+                Personal Details
+              </h2>
             </Card>
+            <TableContainer component={Paper}>
+              <Table className="" sx={{ minWidth: 650,  }} style={{minHeight:"10px"}} aria-label="simple table">
+                <TableHead >
+                  <TableRow style={{alignItems:"center"}}>
+                    <TableCell align="left" style={{border:"none"}}>Calories</TableCell>
+                    <TableCell align="left" style={{border:"none"}}>Fat&nbsp;(g)</TableCell>
+                    <TableCell align="left" style={{border:"none"}}>Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="left" style={{border:"none"}}>Protein&nbsp;(g)</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+            <TableContainer component={Paper} style={{marginTop:"20px"}}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                {/* <TableHead> */}
+                  {/* <TableRow>
+                    <TableCell align="left">Calories</TableCell>
+                    <TableCell align="left">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="left">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="left">Protein&nbsp;(g)</TableCell>
+                  </TableRow> */}
+                {/* </TableHead> */}
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell align="left">{row.calories}</TableCell>
+                      <TableCell align="left">{row.fat}</TableCell>
+                      <TableCell align="left">{row.carbs}</TableCell>
+                      <TableCell align="left">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
         </Row>
       </Container>
